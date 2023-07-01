80cdfd540c28560a195f280a8400cb0a3c6c9bb6
ei1923 <221205093@ccmailg.meijo-u.ac.jp>
2023-07-02 00:02:11 +0900

    abc用のフォルダを作成するスクリプト(文字化けは治せない)

---
+++ makefile.ps1
@@ -0,0 +1,10 @@
+$input = Read-Host "How much count contest?";
+mkdir abc$input;
+
+cp ../MyLibrary/template.cpp ./abc$input/a.cpp;
+cp ../MyLibrary/template.cpp ./abc$input/b.cpp;
+cp ../MyLibrary/template.cpp ./abc$input/c.cpp;
+cp ../MyLibrary/template.cpp ./abc$input/d.cpp;
+cp ../MyLibrary/template.cpp ./abc$input/e.cpp;
+cp ../MyLibrary/template.cpp ./abc$input/f.cpp;
+cp ../MyLibrary/template.cpp ./abc$input/g.cpp;
